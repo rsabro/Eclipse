3b563f8c2379dd0942af66f59ee101b06aaa8cd5
srg2806 <srg2806@NB63680.serasa.intranet>
2019-01-15 15:34:50 -0200

    Adicionei mais uma linha "Data"

--- Hello GitHub/src/br/com/sabro/git/HelloGit.java
+++ Hello GitHub/src/br/com/sabro/git/HelloGit.java
@@ -12,6 +12,7 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git");
 		System.out.println("Professor Sabro");
+		System.out.println("Hoje é 15/01/2019");
 	}
 
 }
